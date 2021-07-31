e420fc1d52dffed4f30ef02f8d4e82ed7c008737
oktay.cekmez <oktay.cekmez@i2i-systems.com>
2021-07-31 15:46:43 +0300

    groupedwithing

--- src/main/java/com/barons/akka/Main.java
+++ src/main/java/com/barons/akka/Main.java
@@ -2,32 +2,64 @@ package com.barons.akka;
 
 import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.japi.function.Function;
 import akka.stream.*;
 import akka.stream.javadsl.*;
 
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.stream.Stream;
 
 public class Main {
-
-
+    public static CompletionStage<Integer> eventHandler(Integer in) throws InterruptedException {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "-Processing event number " + in + "...");
+            return in;
+        });
+    }
 
     public static void main(String[] args) throws InterruptedException {
 
         ActorSystem system = ActorSystem.create();
 
 
+        final Source<Integer, NotUsed> events =
+                Source.fromIterator(() -> Stream.iterate(1, i -> i + 1).iterator()).limit(10);
+
+
+        events
+                .mapAsync(3, Main::eventHandler)
+                .map(in -> "`mapSync` emitted event number ")
+                .runWith(Sink.foreach(System.out::println), system);
+
         int bufferSize = 10;
         int elementsToProcess = 5;
+        CompletionStage stage = CompletableFuture.runAsync(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+        final CompletableFuture<Integer> futureInt = new CompletableFuture<>();
 
         BoundedSourceQueue<Integer> sourceQueue =
-                Source.<Integer>queue(1000000)
-                        .map(x -> x * x).groupedWithin(3,Duration.ofMillis(100))
-                        .to(Sink.foreach(x -> System.out.println("got: " + x)))
+                Source.<Integer>queue(1000000).groupedWithin(6, Duration.ofMillis(100)).flatMapConcat(a -> Source.from(a).grouped(2))
+                        .mapAsyncUnordered(3, a -> {
+                            Thread.sleep(2000);
+                            System.out.println(a);
+                            return CompletableFuture.completedFuture(null);
+                        }).to(Sink.ignore())
                         .run(system);
 
-        List<Integer> fastElements = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,16,17);
+        List<Integer> fastElements = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
 
         fastElements.stream()
                 .forEach(
